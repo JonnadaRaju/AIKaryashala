50f407595f4f572a0eaa5642de6b32b2b185dc6c
Jonnada Raju <22135A1204@gvpce.ac.in>
2026-02-14 04:54:39 +0000

    Modified required changes

--- clang/add_n_n_times.c
+++ clang/add_n_n_times.c
@@ -5,9 +5,9 @@ step-3: read Count
 step-4: Counter = 1
 step-5: Sum = 0
 step-6: if Counter <= Count:
-            Sum = Sum + Count
-            Counter = Counter + 1
-            goto step-6
+          Sum = Sum + Count
+          Counter = Counter + 1
+          goto step-6
 step-7: print "The sum of " + Count + " added itself repeatedly " + Count + " times is " + Sum +".\n"
 step-8: stop */
 
