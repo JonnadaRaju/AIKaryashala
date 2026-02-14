5505f2de943fa3ea8b0c7ccdb8e22edd720fd9ce
Jonnada Raju <22135A1204@gvpce.ac.in>
2026-02-14 04:56:31 +0000

    Modified required changes

--- clang/prime_number.c
+++ clang/prime_number.c
@@ -1,10 +1,28 @@
+/* Algorithm To check Prime or Not.
+step-1: start
+step-2: print "Enter the number to check whether it is prime or not: "
+step-3: read Number
+step-4: NumberCounter = 0
+step-5: Counter = 0
+step-6: if (NumberCounter <= Number):
+          NumberCounter = NumberCounter + 1
+          if (Number % NumberCounter == 0):
+            Counter = Counter + 1
+          goto step-6
+step-7: if (Counter == 2):
+          print Number + " is a prime number.\n"
+step-8: if (Counter != 2):
+          print Number + " is not a prime number.\n"
+step-9: stop */
+
+
 #include<stdio.h>
 
 int main()
 {
 	int number, counter, number_counter;
 
-	printf("Enter the number: ");
+	printf("Enter the number to check whether it is prime or not: ");
 	scanf("%i", &number);
 	number_counter = 0, counter = 0;
 	while(number_counter <= number)
@@ -17,11 +35,11 @@ int main()
 	}
 	if(counter == 2)
 	{
-		printf("%i is prime number.\n", number);
+		printf("%i is a prime number.\n", number);
 	}
 	if(counter != 2)
 	{
-		printf("%i is not prime number.\n", number);
+		printf("%i is not a prime number.\n", number);
 	}
 
 	return 0;
